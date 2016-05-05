541bde062089fad6f1ffaf0ec835bca5a1ff734c
Tuvshinbat <ggakillis@yahoo.com>
2016-05-05 23:12:47 +0900

    Datepicker нэмэв
    Datepicker нэмэв

--- resources/views/includes/modals.blade.php
+++ resources/views/includes/modals.blade.php
@@ -473,15 +473,45 @@
                                     <div  class="col-md-4">
                                       <div class="container">
                                         <div class="row">
-                                            <div class='col-sm-6'>
-                                                <input type='text' class="form-control" id='datetimepicker4' />
+                                            <div class='col-md-3'>
+                                                <div class="form-group">
+                                                    <div class='input-group date' id='datetimepicker6'>
+                                                        <input type='text' class="form-control" />
+                                                        <span class="input-group-addon">
+                                                            <span class="glyphicon glyphicon-calendar"></span>
+                                                        </span>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <script type="text/javascript">
-                                                $(function () {
-                                                    $('#datetimepicker4').datetimepicker();
-                                                });
-                                            </script>
+                                            <div class='col-md-3'>
+                                                <div class="form-group">
+                                                    <div class='input-group date' id='datetimepicker7'>
+                                                        <input type='text' class="form-control" />
+                                                        <span class="input-group-addon">
+                                                            <span class="glyphicon glyphicon-calendar"></span>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
+                                        <script type="text/javascript">
+                                          $(function () {
+                                              $('#datetimepicker6').datetimepicker({
+                                            format: "DD/MM/YYYY"
+                                          });
+                                              $('#datetimepicker7').datetimepicker({
+                                                  useCurrent: false,
+                                            format: "DD/MM/YYYY"
+                                              });
+                                              $("#datetimepicker6").on("dp.change", function (e) {
+                                                  $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+                                              });
+                                              $("#datetimepicker7").on("dp.change", function (e) {
+                                                  $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+                                              });
+                                          
+                                          });
+                                      </script>
                                       </div>
                                     </div>
                                     <div  class="col-md-4">
@@ -530,3 +560,4 @@
                             </div>
                           </div>
     </div>
+    
