d89271f739f68c93b97262fb0452da2343f0fee6
Tuvshinbat <ggakillis@yahoo.com>
2016-05-07 18:06:28 +0900

    Responsive хуваалт зассан

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,30 +4,30 @@
         <div class="right_col" role="main">
           <!-- top tiles -->
           <div class="row tile_count">
-            <div class="col-md-12">
+            <div class="col-md-12 col-xs-12 col-sm-12">
               <div class="x_panel tile">
                 <div class="row x_title">
                   <div class="col-md-12">
                   <h3>Дансний мэдээлэл</h3>
                   </div>
                 </div>
-                  <a class="col-md-2 col-md-offset-1 tile_stats_count" data-toggle="modal" href="#AccountDetail">
+                  <a class="col-md-2 col-md-offset-1 tile_stats_count col-sm-4 col-centered col-xs-12" data-toggle="modal" href="#AccountDetail">
                     <span class="count_top"><i class="fa fa-money"></i> Бэлэн мөнгө</span>
                     <div class="count">{{$accounts->cashEnd}} ₮</div>
                   </a>
-                  <a class="col-md-2 tile_stats_count" data-toggle="modal" href="#AccountDetail">
+                  <a class="col-md-2 tile_stats_count col-sm-4 col-centered col-xs-12" data-toggle="modal" href="#AccountDetail">
                     <span class="count_top"><i class="fa fa-clock-o"></i> Урамшуулал</span>
                     <div class="count">{{$accounts->bonusEnd}} ₮</div>
                   </a>
-                  <a class="col-md-2 tile_stats_count" data-toggle="modal" href="#AccountDetail">
+                  <a class="col-md-2 tile_stats_count col-sm-4 col-centered col-xs-12" data-toggle="modal" href="#AccountDetail">
                     <span class="count_top"><i class="fa fa-trophy"></i> Шагнал</span>
                     <div class="count">{{$accounts->awardEnd}} ₮</div>
                   </a>
-                  <a class="col-md-2 tile_stats_count" data-toggle="modal" href="#AccountDetail">
+                  <a class="col-md-2 tile_stats_count col-sm-6 col-centered col-xs-12" data-toggle="modal" href="#AccountDetail">
                     <span class="count_top"><i class="fa fa-user"></i> Хэрэглээ</span>
                     <div class="count">{{$accounts->usageEnd}} ₮</div>
                   </a>
-                  <a class="col-md-2 tile_stats_count" data-toggle="modal" href="#AccountDetail">
+                  <a class="col-md-2 tile_stats_count col-sm-6 col-centered col-xs-12" data-toggle="modal" href="#AccountDetail">
                     <span class="count_top"><i class="fa fa-hourglass-half"></i> Хуримтлал</span>
                     <div class="count">{{$accounts->savingEnd}} ₮</div>
                   </a>
@@ -36,15 +36,15 @@
           </div>
           <!-- /top tiles -->
         <div class="row">
-          <div class="col-md-8">
+          <div class="col-md-8 col-xs-12 col-sm-12">
             <div class="x_panel tile fixed_height_450">
               <div class="row x_title">
-                <div class="col-md-12">
+                <div class="col-md-12 col-xs-12 col-sm-12">
                 <h3>Блокын мэдээлэл</h3>  
                 </div>
               </div>
               <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-12 col-xs-12 col-sm-12">
                   <ul id="hexGrid">
                     <div>
                       <li class="hex">
@@ -159,7 +159,7 @@
               </div>
              </div>
           </div>
-          <div class="col-md-4">
+          <div class="col-md-4 col-sm-12 col-xs-12">
               <div class="x_panel tile fixed_height_450">
               <div class="row x_title">
                 <div class="col-md-12">
