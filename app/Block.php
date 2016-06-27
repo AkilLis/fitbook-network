d6469aca472ef08a27b6f15a114ea3851c920be3
AkilLis <g.tuvshinbat@yahoo.com>
2016-06-27 21:41:36 +0900

    test

--- app/Block.php
+++ app/Block.php
@@ -9,9 +9,18 @@ class Block extends BaseModel
     //
     protected $primaryKey = 'id';
 	protected $table = 'block';
+	public $members;
 
-	public function members()
+	public function members_test()
 	{
-		return $this->belongsToMany('App\User', 'userblockmap', 'blockId', 'userId')->withPivot('fCount', 'mCount', 'created_at','viewOrder');
+		return $this->belongsToMany('App\User', 'userblockmap', 'blockId', 'userId')->withPivot('fCount', 'created_at','viewOrder');
+	}
+
+	public function setMembers()
+	{
+		$this->members = \DB::table('users')->join('userblockmap', 'userblockmap.userId','=','users.id')
+			 ->where('userblockmap.blockId','=', $this->id)
+			 ->select('users.id', 'users.userId', 'users.fName', 'users.lName', 'userblockmap.fCount', 'userblockmap.created_at', 'userblockmap.viewOrder')
+			 ->get();
 	}
 }
