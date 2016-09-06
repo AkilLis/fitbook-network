ba5b89fea267ff0b602b5b1340a45b5f856aa611
AkilLis <g.tuvshinbat@yahoo.com>
2016-09-06 12:31:19 +0900

    promudtion

---
+++ public/app/controller/promcontroller.js
@@ -0,0 +1,38 @@
+app.controller('promCtrl', function($scope, $uibModal, $http, $log) { 
+
+  	$isproduction = false;
+    $baseUrl = $isproduction ? 'http://flexgym.mn/' : 'http://localhost/';
+    $scope.users = {};
+
+  	$scope.openModal = function(id) {
+      $scope.$modalInstance = $uibModal.open({
+          templateUrl: id + '.html',
+          animation: true,
+          size: 'sm',
+          scope: $scope
+      })
+    }
+
+    $scope.ok = function() {
+        $scope.$modalInstance.close();
+    };
+
+    $scope.cancel = function() {
+        $scope.$modalInstance.dismiss('cancel');
+    };
+
+    $scope.childs = function(id) {
+        $http({
+            method: 'GET',
+            url: $baseUrl + 'promution/' + id,
+            }).then(function successCallback(response) 
+            {
+              $scope.childsList = response.data.result;
+              $scope.openModal('AddPromutionUser');
+            }, function errorCallback(response) 
+            {
+
+            }
+        ); 
+    };
+});
